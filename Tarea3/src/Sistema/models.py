5acf6f793a1e855d1ee9d6d60e34c4e380b37b3a
swsandra <sandrav0609@gmail.com>
2018-05-15 21:13:55 -0400

    Funcion de verificar correo.

--- Tarea3/src/Sistema/models.py
+++ Tarea3/src/Sistema/models.py
@@ -5,7 +5,12 @@ import re
 
 class Seguridad:
     def registrarUsuario(self, correo, clave1, clave2):
-        
+        try:
+            assert(re.match("^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$",correo,flags=0)!=None)
+            pass
+        except :
+            print("Correo electrónico inválido")
+            exit()
         try:
             assert(clave1==clave2)
         except:
