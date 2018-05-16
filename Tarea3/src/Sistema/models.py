2e4319eed2d6a65e9f2b7041684522d719535538
Giulianne17 <gvtr96@gmail.com>
2018-05-16 12:59:35 -0400

    Agregado assert de cantidad de digitos y de letras minusculas.

--- Tarea3/src/Sistema/models.py
+++ Tarea3/src/Sistema/models.py
@@ -17,8 +17,50 @@ class Seguridad:
             # Su longitud debe estar entre 8 y 16 caracteres
             assert(8<=len(clave1)<=16)
             
+                        
+            # Su longitud debe estar entre 8 y 16 caracteres
+            assert(8<=len(clave1)<=16)
+
+            # No debe incluir caracteres especiales.
+            assert((clave1.isalnum()!=False))
+            
+            #Al menos tres letras y al menos una de ellas debe ser mayúscula y una minúscula
+            #Debe contener al menos un dígito.
+            digito1 = 0
+            totalLetras1 = 0
+            cantidadMayusculas1=0
+            cantidadMinusculas1=0
+            for i in range(len(clave1)):
+                if (clave1[i].isdigit()):
+                    digito1+=1
+                elif (clave1[i].isupper()):
+                    cantidadMayusculas1 +=1
+                elif (clave1[i].islower()):
+                    cantidadMinusculas1 +=1
+            
+            totalLetras1=cantidadMayusculas1+cantidadMinusculas1
+            
+            #Revisando minimo de letras, de mayusculas, de minusculas y de digitos
+
+            assert(totalLetras1 >= 3)
+            assert(cantidadMayusculas1!=0 and cantidadMinusculas1!=0)
+            assert(digito1 >= 1)
+
+            return 7
+            
         except:
             if (clave1 != clave2):
                 return 0
             elif (8>len(clave1) or len(clave1)>16):
                 return 1
+            elif (clave1.isalnum()==False):
+                return 2
+            elif (totalLetras1<3):
+                return 3
+            elif (cantidadMayusculas1 == 0):
+                return 4
+            elif (cantidadMinusculas1 == 0):
+                return 5
+            elif (digito1==0):  
+                return 6
+        
